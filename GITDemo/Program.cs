d5bb07837206be579e768807038eb7a2114b2b9c
vinivelozo <Leonicio@LAPTOP-95I55FRH>
2021-09-20 15:32:25 -0400

    Add project files.

---
+++ GITDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GITDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Write a number and I'll tell you if is's a multiple of 10");
+
+                string response = Console.ReadLine();
+
+                if (Int32.TryParse(response, out int outVal))
+                {
+
+
+                    if(outVal % 10 == 0)
+                    {
+                        Console.WriteLine("It is a multiple of 10");
+
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("It is not a multiple of 10");
+                    }
+                }
+
+            }
+
+        }
+    }
+}
